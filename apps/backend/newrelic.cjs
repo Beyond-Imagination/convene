963b9b640871f6e2821fd8914484154fb7712a96
Jaehyun Yoon <yuchem2@gmail.com>
2026-06-29 20:57:05 +0900

    chore: New Relic APM 도입 (#10)

---
+++ apps/backend/newrelic.cjs
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * New Relic agent 설정.
+ */
+exports.config = {
+  app_name: 'convene-backend',
+  agent_enabled: Boolean(process.env.NEW_RELIC_LICENSE_KEY),
+  distributed_tracing: {
+    enabled: true,
+  },
+  logging: {
+    level: process.env.NEW_RELIC_LOG_LEVEL || 'info',
+  },
+  application_logging: {
+    enabled: true,
+    forwarding: {
+      enabled: true,
+    },
+    local_decorating: {
+      enabled: false,
+    },
+    metrics: {
+      enabled: true,
+    },
+  },
+  attributes: {
+    exclude: [
+      'request.headers.authorization',
+      'request.headers.cookie',
+      'request.headers.setCookie',
+      'response.headers.setCookie',
+    ],
+  },
+};
